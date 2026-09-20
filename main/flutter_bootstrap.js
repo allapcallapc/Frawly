227348f9823684aafb813c952de530112ce377d2
allapcallapc <allapcallapc@users.noreply.github.com>
2026-09-20 02:26:58 +0000

    deploy: main @ f83b709d5d06bd78a4a805e45accd6ae0cede6c0 f83b709d5d06bd78a4a805e45accd6ae0cede6c0

--- main/flutter_bootstrap.js
+++ main/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "353498934" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2772356039" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
